b1cfab9711c98ef65a0fcc4b01b11674a8ad442a
thiagocomel <thiago.comel@gmail.com>
2020-10-01 14:47:56 -0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Cliente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Cliente
+    {
+        public long Indentificacao;
+        public string Nome;
+        public double TicketMedio;
+        public string FaixaTicketMedio;
+        public DateTime UltimaVenda;
+        public string Sexo;
+        public string TipoPessoa;
+        public List<Venda> ListaVendas = new List<Venda>();
+        public bool RevisaoAtrasada;
+    }
+
+    public class Venda
+    {
+        public int IDNF;
+        public DateTime Data;
+        public double Valor;
+        public int QntItens;
+        public double VlrItens;
+        public double CustoItens;
+        public string DadosAdicionais;
+        public string Veiculo;
+        public string Placa;
+        public string KM;
+        public string Ano;
+        public string FaixaAno;
+    }
+
+    public class Servico
+    {
+        public int IDNF;
+        public string TipoServico;
+        public string NomeServico;
+        public string Funcionario;
+        public string Tempo;
+        public double VlrUnit;
+        public int QntItens;
+        public double VlrTotal;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/daoFireBird.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FirebirdSql.Data.FirebirdClient;
+using System.Configuration;
+
+namespace fireBirdWin
+{
+    /// <summary>
+    /// Usa padrão Singleton para obter uma instancia do FireBird
+    /// </summary>
+    public class daoFireBird
+    {
+        private static readonly daoFireBird instanciaFireBird = new daoFireBird();
+
+        private daoFireBird() { }
+
+        public static daoFireBird getInstancia()
+        {
+            return instanciaFireBird;
+        }
+
+        public FbConnection getConexao()
+        {
+            string conn = @"DataSource=localhost; Database=F:\BKP\Particular\Irmãos Neto\Dados.FDB; UserId=SYSDBA; Pwd=masterkey";
+            return new FbConnection(conn);
+        }
+    }
+}
